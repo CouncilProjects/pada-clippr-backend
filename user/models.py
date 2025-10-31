9651c69556b09b9ce06a75cbca332f44e6cede1f
MihalisTs <micaheltsavos@gmail.com>
2025-10-31 20:23:16 +0200

    Added DRF

--- user/models.py
+++ user/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     # AbstractUser already includes: username, email, password, first_name, last_name
-    is_seller = models.BooleanField(default=False)
     phone=models.CharField(max_length=20, blank=True)
 
     #for seller
