7542f1668c7086856ddf747df90dc4a734ccee1a
Martin Andronikos <martin.andronikos@gmail.com>
2025-11-02 21:51:13 +0200

    Migrations + Working JWT Endpoints

--- user/views.py
+++ user/views.py
@@ -1,1 +1,8 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class Register(APIView):
+    def post(self, request):
+        return Response({
+            'register': 69
+        })
