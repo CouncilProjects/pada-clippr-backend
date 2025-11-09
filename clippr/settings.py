4e46e0444f16ff084a4a6482ea2c40b480b01eaf
CouncilProjects <amorianosdimitris@gmail.com>
2025-11-09 16:22:19 +0200

    made sure BE accepts credentials in cookies

--- clippr/settings.py
+++ clippr/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
